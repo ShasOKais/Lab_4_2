28abfa370c4091ef6bc1bb371422b4c544130062
ShasOKais <emil49@yandex.ru>
2020-11-14 12:22:49 +0500

    Добавление идей без MVC

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,24 +16,32 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_spinBox_valueChanged(int arg1)
 {
-    data.setValue(arg1);
-    update();
+    //data.setValue(arg1);
+    ui->spinBox->setValue(arg1);
+    ui->progressBar->setValue(arg1);
+    ui->lineEdit->setText(QString::number(arg1));
+    //update();
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_clicked() // при нажатии кнопки выводит сообщение
 {
-    msg.about(this, "Окно95", QString::number(data.getValue()));
-    update();
+    //msg.about(this, "Окно95", QString::number(data.getValue()));
+    msg.about(this, "NoMVC", QString::number(ui->spinBox->value()));
+    //update();
 }
 
 
-void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_textChanged(const QString &arg1) // принимает аргумент и заноси его в Data
 {
-    data.setValue(ui->lineEdit->text().toInt());
-    update();
+    //data.setValue(ui->lineEdit->text().toInt());
+    int arg = arg1.toInt();
+    ui->spinBox->setValue(arg);
+    ui->progressBar->setValue(arg);
+    ui->lineEdit->setText(arg1);
+    //update();
 }
 
-void MainWindow::update()
+void MainWindow::update() // обновляет форму после введенеия данных
 {
     ui->spinBox->setValue(data.getValue());
     ui->lineEdit->setText(QString::number(data.getValue()));
